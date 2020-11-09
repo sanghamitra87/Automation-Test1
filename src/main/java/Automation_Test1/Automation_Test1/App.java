8d2aac4e7dac9c66a6b9fc9ecd4703ee15dc3e70
user <user@DESKTOP-12FHLFS>
2020-11-09 12:29:35 +0530

    commit

---
+++ src/main/java/Automation_Test1/Automation_Test1/App.java
@@ -0,0 +1,13 @@
+package Automation_Test1.Automation_Test1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
